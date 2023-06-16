8b79afe44b359d0a7e9d2976900632451e1edfe5
gokul <gokulchandan18@gmail.com>
2023-06-16 16:38:28 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
-import Asd from "./components/Asd";
 import Zxcv from "./components/Zxcv";
 
 function App() {
   return (
     <div className="App">
       <Zxcv />
-      <Asd />
+   
     </div>
   );
 }
