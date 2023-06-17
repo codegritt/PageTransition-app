5ba24294efa56e6f8f02a49b788c1049aa5504b7
gokul <gokulchandan18@gmail.com>
2023-06-17 12:40:14 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Rafce from "./components/Rafce";
 import Zxcv from "./components/Zxcv";
 
 function App() {
   return (
     <div className="App">
       <Zxcv />
+      <Rafce/>
    
     </div>
   );
---
+++ src/components/Rafce.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Rafce = () => {
+  return (
+    <div style={{border:"2px solid black",width:"100px",height:"100px",position:"relative",
+    left:"200px"}}>
+        <div style={{fontSize:"850px",position:"relative",
+    bottom:"903px",right:"45px"}}>
+            .
+        </div>
+    </div>
+  )
+}
+
+export default Rafce
